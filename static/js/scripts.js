3eaa18a73828e43b26088e0c6bcf0327e1b4f57a
Raiane <raianw200410@outlook.com>
2024-05-10 00:03:39 -0300

    salavndo lista com checkbox

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,6 +7,29 @@
 // Scripts
 // 
 
+const selectBtn = document.querySelector(".select-btn"),
+      items = document.querySelectorAll(".item");
+
+selectBtn.addEventListener("click", () => {
+    selectBtn.classList.toggle("open");
+});
+
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("checked");
+
+        let checked = document.querySelectorAll(".checked"),
+            btnText = document.querySelector(".btn-text");
+
+        if(checked && checked.length > 0) {
+            btnText.innerText = `${checked.length} Selected`; // Correção aqui
+        } else {
+            btnText.innerText = "Select Language";
+        }
+    });
+});
+
+
 // ocultar ou não a barra de navegação.
 window.addEventListener('DOMContentLoaded', event => {
 
